6beb38ea865fe2b0288fccaffbabcfc32e0acb6a
Kenji Hirabayashi <mkhira2@gmail.com>
2017-02-23 14:16:38 -0600

    initial commit

---
+++ js/script.js
@@ -0,0 +1,89 @@
+console.log($)
+var searchURL = 'https://congress.api.sunlightfoundation.com/bills/search?callback=?&apikey=123&query='
+var baseURL = 'https://congress.api.sunlightfoundation.com/bills?callback=?&apikey=123'
+
+// getJSON is a method that takes the input url and makes a request to the server at that location
+// getJSON returns a promise that will resolve when the response to the request comes in
+
+var makeBillHTML = function(singleBillObj) { // takes in a single object of the array and converts the object into html
+    var getHTML = ''
+    getHTML += '<div class="bill">'
+    getHTML += '<h2 class="title">' + singleBillObj.official_title + '</h2>'
+    getHTML += '<p class="chamber"> Chamber: ' + singleBillObj.chamber + '</p>'
+    getHTML += '<p class="sponsor"> Sponsor: ' + singleBillObj.sponsor.first_name + ' ' + singleBillObj.sponsor.last_name + '</p>'
+    getHTML += '<p class="status"> Status: ' + (singleBillObj.history.active ? 'active' : 'dead') + '</p>'
+    getHTML += '<p class="introDate"> Introduced on: ' + singleBillObj.introduced_on + '</p>'
+    getHTML += '</div>'
+
+    return getHTML
+
+}
+
+function handleBillsResponse(billsObj) { // takes object from apiresponse and processes data each iteration
+    var allBillsHTML = ''
+    var containerNode = document.querySelector('.container')
+
+    for (var i = 0; i < billsObj.results.length; i++) {
+        allBillsHTML += makeBillHTML(billsObj.results[i])
+    }
+    containerNode.innerHTML = allBillsHTML
+    hideGif()
+}
+
+function hideGif() { // hides loading gif once bills are displayed
+    var loadingIcon = document.querySelector('.loading_div')
+    loadingIcon.style.display = 'none'
+}
+
+//--------------------------------------------------
+// SEARCH BAR
+//--------------------------------------------------
+
+function getBills(url) {
+    var billsPromise = $.getJSON(url)
+    billsPromise.then(handleBillsResponse)
+}
+
+var inputSearch = document.querySelector('.searchBar')
+
+inputSearch.addEventListener('keydown', function(eventObj) {
+    if (eventObj.keyCode === 13) {
+        var fullURL = searchURL + eventObj.target.value
+        getBills(fullURL)
+    }
+})
+
+function showHome() {
+    var containerNode = document.querySelector('.container')
+    containerNode.innerHTML = '<img src=http://i.imgur.com/u2gQ5yK.gif>'
+}
+
+var showBillSearch = function() {
+    getBills(baseURL)
+}
+
+function router() {
+    if (location.hash === '#home') {
+        showHome()
+    } 
+
+    else if (location.hash === '#billSearch') {
+        showBillSearch()
+    }
+
+    else {
+        location.hash = 'home'
+    }
+}
+
+window.addEventListener('hashchange', router)
+
+router()
+
+
+
+
+
+
+
+
